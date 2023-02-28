c2e94942a9a0edf3ddd1903dbc6f36ec5839c48c
ali <aliahmad2951123@gmail.com>
2023-02-28 05:10:20 +0200

    Add bootstrap and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  # root "articles#index"
+   root 'pages#home'
+   get 'about', to: 'pages#about'
+  # get 'test', to: 'articles#test'
+  # resources :articles , only: [ :index, :new, :create,:update,:edit,:test]# articles/1
+  #  delete 'articles/:id' , to: 'articles#destroy'
+  # delete 'articles/:id/delete' => 'articles#destroy', as: 'articles_delete'
+  # get '/articles/:id/delete' => 'articles#destroy'
   resources :articles
+=begin
+  REST - Representational state transfer - mapping HTTP verbs (get, post, put/patch, delete)  
+  to CRUD actions 
+    resources porvides REST-ful routes to rails resources 
+=end
 end
