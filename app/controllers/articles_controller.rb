68ab334a44d4caee30f8a8375f06ad5171f9e2e7
aliahmed1881 <aliahmed2951123@gmail.com>
2023-03-04 09:37:01 +0200

    second edittion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,6 +56,7 @@ class ArticlesController < ApplicationController
 
   
 =begin  
+after edit
     # @article = Article.new(params.require(:article).permit(:title, :description))  
     # if  @article.save
     # flash[:notice] = "Article was created successfully. "
